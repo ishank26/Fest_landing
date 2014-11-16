c159969665edf0eb2a3705a8ab467a870cdc470f
ishank26 <ishank269@gmail.com>
2014-11-16 20:54:55 +0530

    init

---
+++ js/gal.js
@@ -0,0 +1,17 @@
+$(function(){
+        $.mbBgndGallery.buildGallery({
+                containment:"body",
+                timer:3000,
+                effTimer:2000,
+                effect:"zoom",
+                grayScale:false,
+               images:[
+                 "images/1.jpg",
+                 "images/2.jpg",
+                 "images/3.jpg",
+                 "images/4.jpg",
+                 ],
+
+});
+})        
+
